6f53d62e580a8e1e24a945339341b190c4706b6e
Doc <65479309+DocilElm@users.noreply.github.com>
2025-03-20 00:38:08 -0700

    LocalStore: add data backup

--- storage/LocalStore.js
+++ storage/LocalStore.js
@@ -2,18 +2,22 @@ const cachedInstances = []
 
 /**
  * - This data is meant to be edited on the fly as well as gathered from a file
+ * - Note: The data is backed up in a folder inside of `.minecraft/config/tska-backup` every 10minutes
+ * and whenever the file gets corrupted it attempts to re-set it with the backup one
  */
 export class LocalStore {
     constructor(moduleName, defaultData = {}, fileName = ".data.json") {
-        const cachedData = FileLib.read(moduleName, fileName)
-        if (!cachedData) console.warn(`[TSKA] Seems like your data for module \"${moduleName}\" was corruputed therefore resetted.`)
+        let cachedData = FileLib.read(moduleName, fileName) || FileLib.read(`./config/tska-backup/${moduleName}/${fileName}`)
+        if (!cachedData && FileLib.exists(moduleName, fileName))
+            console.warn(`[TSKA] Seems like your data for module \"${moduleName}\" was corruputed therefore resetted.`)
 
-        const parsed = JSON.parse(cachedData ?? "{}")
+        const parsed = JSON.parse(cachedData || "{}")
         Object.assign(this, defaultData, parsed)
 
         /**
          * - Function that returns the module's `moduleName` and `fileName`
          * to store the data at
+         * @private
          * @returns {[string, string]}
          */
         this.getModuleData = () => {
@@ -34,9 +38,34 @@ export class LocalStore {
 
         FileLib.write(moduleName, fileName, JSON.stringify(this, null, 4), true)
     }
+
+    /** @private */
+    _saveBackup() {
+        const [ moduleName, fileName ] = this.getModuleData()
+
+        FileLib.write(`./config/tska-backup/${moduleName}/${fileName}`, JSON.stringify(this, null, 4), true)
+    }
 }
+
+const data = new LocalStore("tska", {
+    LocalStore: {
+        lastBackup: null
+    }
+}, "data/data.json")
 
 register("gameUnload", () => {
     for (let local of cachedInstances)
         local.save()
+})
+
+register("tick", () => {
+    if (!World.isLoaded()) return
+    const lastBackup = data.LocalStore.lastBackup
+    if (lastBackup && Date.now() - lastBackup <= 1000 * 60 * 10) return
+
+    data.LocalStore.lastBackup = Date.now()
+    new Thread(() => {
+        for (let local of cachedInstances)
+            local._saveBackup()
+    }).start()
 })
