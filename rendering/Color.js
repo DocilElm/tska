6ce4dec3c79a695290568e9e852df9b05c3609c7
Cameron Lee <127889040+nwjn@users.noreply.github.com>
2025-03-21 23:09:51 -0700

    Color: Simplify wrapper

--- rendering/Color.js
+++ rendering/Color.js
@@ -1,5 +1,3 @@
-const JavaColor = Java.type("java.awt.Color")
-
 export class Color {
     /** @param {[number, number, number, number]} rgbaArray */
     constructor(rgbaArray) {
@@ -10,21 +8,6 @@ export class Color {
         this._recompute()
     }
 
-    /** @returns {[number, number, number, number]} 4 ints between [0, 255] */
-    getRGBA() {
-        return Array.from(this.u8View.values())
-    }
-
-    /** @returns {number} [0xRRGGBBAA] int between [0, 2^32) */
-    getHex() {
-        return this.u32View[0]
-    }
-
-    /** @returns {JavaTColor} */
-    getJavaColor() {
-        return new JavaColor(this.u8View[0], this.u8View[1], this.u8View[2], this.u8View[3])
-    }
-
     /**
      * - Use only for drawing strings
      * - This hex is formatted as [0xAARRGGBB] instead of [0xRRGGBBAA]
@@ -32,17 +15,6 @@ export class Color {
      */
     getColorForFont() {
         return this.argbHex
-    }
-
-    /**
-     * - Transform the color to the hex/int
-     * @param {number} u32int [0xRRGGBBAA] int between [0, 2^32)
-     */
-    setFromHex(u32int) {
-        this.u32View[0] = u32int
-        this.u8View.reverse()
-        
-        this._recompute()
     }
 
     /**
@@ -61,19 +33,13 @@ export class Color {
      * @param {?number} alpha scaled float between [0, 1]
      */
     glColor(alpha = this.f32View[3]) {
-        GlStateManager./* color */func_179131_c(
-            this.f32View[0],
-            this.f32View[1],
-            this.f32View[2],
-            alpha
-        )
+        GlStateManager./* color */func_179131_c(this.f32View[0], this.f32View[1], this.f32View[2], alpha)
     }
 
     /** @private @internal*/
     _recompute() {
         this.f32View = Array.from(this.u8View.values(), c => c / 0xff)
 
-        const argb = new Uint8ClampedArray([this.u8View[3], this.u8View[0], this.u8View[1], this.u8View[2]])
-        this.argbHex = new Uint32Array(argb.buffer)[0]
+        this.argbHex = new Uint8ClampedArray(new Uint32Array([this.u8View[3], this.u8View[0], this.u8View[1], this.u8View[2]]).buffer)[0]
     }
 }
